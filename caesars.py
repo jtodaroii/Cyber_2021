890da4a0a73caa6dad392a4935365ed94f570477
James Todaro <30529065+jtodaroii@users.noreply.github.com>
2021-06-24 16:25:30 +0000

    Moved functions from main to Caesars.py

---
+++ caesars.py
@@ -0,0 +1,29 @@
+"""
+letter should be a lower case letter from 'a' to 'z'
+"""
+def encrypt_letter(letter, key):
+  plaintext_letter_number = ord(letter) - ord('a')
+  cipher_letter_number = (plaintext_letter_number + key) % 26
+  #print(cipher_letter_number)
+  cipher_letter = chr(cipher_letter_number + ord('a'))
+  return cipher_letter
+
+def encrypt_word(word, key):
+  encrypted_word = ""
+  for letter in word:
+    encrypted_word += encrypt_letter(letter, key)
+  return encrypted_word
+
+def decrypt_letter(letter, key):
+  ciphertext_letter_number = ord(letter) - ord('a')
+  plain_letter_number = (ciphertext_letter_number - key)
+  if plain_letter_number < 0:
+    plain_letter_number += 26
+  plain_letter = chr(plain_letter_number + ord('a')) 
+  return plain_letter
+
+def decrypt_word(word, key):
+  decrypted_word = ""
+  for letter in word:
+    decrypted_word += decrypt_letter(letter, key)
+  return decrypted_word
